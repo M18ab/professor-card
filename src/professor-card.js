175ce873b35f17aef88e978ffa6fe0e8e6687f52
Spenser McLaughlin <mcspenser7@gmail.com>
2023-02-15 23:12:05 -0500

    Professor O is ready to go!

--- src/professor-card.js
+++ src/professor-card.js
@@ -1,86 +1,104 @@
 import { LitElement, html, css } from 'lit';
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+const prof = new URL('../assets/professoro.jpg', import.meta.url).href;
 
-class ProfessorCard extends LitElement {
-  static properties = {
-    header: { type: String },
-  };
+export class ProfessorCard extends LitElement {
+  static get properties() {
+    return {
+      author: {
+        type: String,
+      },
+      professor: {
+        type: String,
+        reflect: true,
+      },
+    };
+  }
 
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--professor-card-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
+  static get styles() {
+    return css`
+      .two {
+        display: flex;
+        flex-direction: row;
       }
-      to {
-        transform: rotate(360deg);
+
+      .card {
+        text-align: center;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        transition: 0.3s;
+        height: auto;
+        width: 300px;
+        background-color: #d3d3d3;
+        display: flex;
+        flex-direction: column;
+        margin: 10px;
+        border-radius: 25px;
       }
-    }
 
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
+      @media screen and (min-width: 800px) {
+        .btn {
+          visibility: hidden;
+        }
+      }
 
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
+      @media screen and (max-width: 500px) {
+        .card {
+          text-transform: var;
+          width: var;
+          font-size: 4vw;
+        }
+      }
+
+      button {
+        text-transform: uppercase;
+        padding: 8px 8px;
+        margin: 10px;
+        border: 2px dashed white;
+        background-color: #a71c19;
+        color: white;
+      }
+
+      img {
+        margin: 25px;
+      }
+
+      /* .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+      } */
+
+      button:hover {
+        border: 2px dashed #a71c19;
+        background-color: white;
+        color: #a71c19;
+      }
+
+      button:focus {
+        border: 2px dashed #a71c19;
+        background-color: white;
+        color: #a71c19;
+      }
+    `;
+  }
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.author = 'Spenser McLaughlin';
+    this.professor = 'Professor Bryan Ollendyke';
   }
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/ProfessorCard.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+      <div>
+        <div class="two">
+          <div class="card" id="card1">
+            <h2 id="header">${this.professor}</h2>
+            <h3>By ${this.author}</h3>
+            <img src="${prof}" alt="btopro" />
+            <p id="paragraph"></p>
+            <button class="btn" id="btn">Details</button>
+          </div>
+        </div>
+      </div>
     `;
   }
 }
