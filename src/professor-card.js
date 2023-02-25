6bf4f91c7733c88cc9fd9b7d96722ef749db3b25
Spenser McLaughlin <mcspenser7@gmail.com>
2023-02-25 18:49:40 -0500

    EsLint won't die

--- src/professor-card.js
+++ src/professor-card.js
@@ -10,7 +10,7 @@ export class ProfessorCard extends LitElement {
       professor: { type: String, reflect: true,},
       funFact: { type: String,},
       topText: { type: String,},
-      BottomText: { type: String,},
+      bottomText: { type: String,},
       profURL: { type: String,},
       description: {type: String}
     };
@@ -18,14 +18,8 @@ export class ProfessorCard extends LitElement {
 
   static get styles() {
     return css`
-      .two {
-        display: flex;
-        flex-direction: row;
-      }
-
       .card {
         text-align: center;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
         transition: 0.3s;
         height: auto;
         width: 300px;
@@ -57,6 +51,7 @@ export class ProfessorCard extends LitElement {
       }
     `;
   }
+  
 
   constructor() {
     super();
@@ -72,22 +67,20 @@ export class ProfessorCard extends LitElement {
   render() {
     return html`
       <div>
-        <div class="two">
-          <div class="card" id="card1">
-            <h2 id="header">${this.professor}</h2>
-            <h3>By ${this.author}</h3>
-            <meme-maker
-              top-text="${this.topText}"
-              image-url="${this.profURL}"
-              bottom-text="${this.bottomText}"
-            ></meme-maker>
-            <details class="details">
-              <summary>${this.description}</summary>
-              <div>
-                <slot name="bing"></slot>
-              </div>
-            </details>
-          </div>
+        <div class="card" id="card1">
+          <h2 id="header">${this.professor}</h2>
+          <h3>By ${this.author}</h3>
+          <meme-maker
+            top-text="${this.topText}"             
+            image-url="${this.profURL}"
+            bottom-text="${this.bottomText}"
+          ></meme-maker>
+          <details class="details" id="details">
+            <summary>${this.description}</summary>
+            <div>
+              <slot name="bing"></slot>
+            </div>
+          </details>
         </div>
       </div>
     `;
