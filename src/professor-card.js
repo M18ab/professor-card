7650b7a36c801530e72173ef8e2ce6db60a1ccbf
Spenser McLaughlin <122754992+M18ab@users.noreply.github.com>
2023-02-26 12:42:14 -0500

    So close, yet so far

--- src/professor-card.js
+++ src/professor-card.js
@@ -6,18 +6,23 @@ const prof = new URL('../assets/professoro.jpg', import.meta.url).href;
 export class ProfessorCard extends LitElement {
   static get properties() {
     return {
-      author: { type: String,},
-      professor: { type: String, reflect: true,},
-      funFact: { type: String,},
-      topText: { type: String,},
-      bottomText: { type: String,},
-      profURL: { type: String,},
-      description: {type: String}
+      author: { type: String},
+      professorName: { type: String, reflect: true},
+      funFact: { type: String},
+      topText: { type: String},
+      bottomText: { type: String},
+      imgURL: { type: String},
+      description: {type: String},
+      shadowColor: {type: String, reflect: true, attribute: 'shadow-color'},
     };
   }
 
   static get styles() {
     return css`
+      :host([shadow-color="true"]) .card {
+        box-shadow: 1px 2px 3px 4px rgba(20,20,20,0.4);
+      }
+
       .card {
         text-align: center;
         transition: 0.3s;
@@ -28,6 +33,7 @@ export class ProfessorCard extends LitElement {
         flex-direction: column;
         margin: 10px;
         border-radius: 25px;
+        box-shadow: ;
       }
 
       /* @media screen and (min-width: 800px) {
@@ -56,23 +62,28 @@ export class ProfessorCard extends LitElement {
   constructor() {
     super();
     this.author = 'Spenser McLaughlin';
-    this.professor = 'Professor Bryan Ollendyke';
+    this.professorName = 'Professor Bryan Ollendyke';
     this.funFact = 'This is a picture of a Bing user';
     this.topText = 'Imagine being IT competent';
     this.bottomText = 'But using BING';
-    this.profURL = 'https://avatars.githubusercontent.com/u/329735?v=4';
+    this.imgURL = 'https://avatars.githubusercontent.com/u/329735?v=4';
     this.description = 'Details';
+    this.shadowColor = "true";
+  }
+
+  toggleDetails() {
+    this.shadowRoot.querySelector('.details').toggleAttribute('open');
   }
 
   render() {
     return html`
       <div>
         <div class="card" id="card1">
-          <h2 id="header">${this.professor}</h2>
+          <h2 id="header">${this.professorName}</h2>
           <h3>By ${this.author}</h3>
           <meme-maker
             top-text="${this.topText}"             
-            image-url="${this.profURL}"
+            image-url="${this.imgURL}"
             bottom-text="${this.bottomText}"
           ></meme-maker>
           <details class="details" id="details">
