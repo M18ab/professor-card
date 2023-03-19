1af834dd6b5e4bfe629583a01b88357ff2ecf8f8
Spenser McLaughlin <mcspenser7@gmail.com>
2023-03-19 09:43:04 -0400

    Goodbye Project 1

--- api/roster.js
+++ api/roster.js
@@ -20,6 +20,36 @@ export default async function handler(request, res) {
             "bottomText": "But using BING",
             "description": "Details",
             "shadowColor": "false"
+        },
+        {
+            "imgURL": "https://media.licdn.com/dms/image/D4D03AQHzfmbMoNNrww/profile-displayphoto-shrink_800_800/0/1671642107740?e=2147483647&v=beta&t=ZC1J3poYWxvIe5KsDFsObVcXQlh2-mv5mhIjjwOOkkk",
+            "author": "Spenser McLaughlin",
+            "professorName": "Professor Bryan Ollendyke",
+            "funFact": "This is a picture of a Bing user",
+            "topText": "Imagine being IT competent",
+            "bottomText": "But using BING",
+            "description": "Details",
+            "shadowColor": "false"
+        },
+        {
+            "imgURL": "https://media.licdn.com/dms/image/D4D03AQHzfmbMoNNrww/profile-displayphoto-shrink_800_800/0/1671642107740?e=2147483647&v=beta&t=ZC1J3poYWxvIe5KsDFsObVcXQlh2-mv5mhIjjwOOkkk",
+            "author": "Spenser McLaughlin",
+            "professorName": "Professor Bryan Ollendyke",
+            "funFact": "This is a picture of a Bing user",
+            "topText": "Imagine being IT competent",
+            "bottomText": "But using BING",
+            "description": "Details",
+            "shadowColor": "false"
+        },
+        {
+            "imgURL": "https://media.licdn.com/dms/image/D4D03AQHzfmbMoNNrww/profile-displayphoto-shrink_800_800/0/1671642107740?e=2147483647&v=beta&t=ZC1J3poYWxvIe5KsDFsObVcXQlh2-mv5mhIjjwOOkkk",
+            "author": "Spenser McLaughlin",
+            "professorName": "Professor Bryan Ollendyke",
+            "funFact": "This is a picture of a Bing user",
+            "topText": "Imagine being IT competent",
+            "bottomText": "But using BING",
+            "description": "Details",
+            "shadowColor": "false"
         }
     ];
     res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
